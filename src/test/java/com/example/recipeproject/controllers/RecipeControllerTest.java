749ce8476a4a372c86279e7f19af9730e16b8244
Zentari <zenti.asteris@gmail.com>
2021-01-13 01:19:09 +0300

    Add exception pages

--- src/test/java/com/example/recipeproject/controllers/RecipeControllerTest.java
+++ src/test/java/com/example/recipeproject/controllers/RecipeControllerTest.java
@@ -53,15 +53,18 @@ class RecipeControllerTest {
 
     @Test
     void testGetRecipeNotFound() throws Exception {
-        Recipe recipe = new Recipe();
-        recipe.setId(1L);
-
-
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
+    }
+
+    @Test
+    void testGetRecipeNumberFormat() throws Exception {
+        mockMvc.perform(get("/recipe/asd/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 
     @Test
